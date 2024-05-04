d2733563b8764da23a177868118d06f6f02c6f4f
gitcoder <kuntalmajee338@gmail.com>
2024-05-04 12:00:30 +0530

    created app

---
+++ src/hooks/useCurrencyInfo.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+function  useCurrencyInfo(currency) {
+    const [data, setData] = useState({});
+    useEffect(() => {
+        fetch(`https://open.er-api.com/v6/latest/${currency.toUpperCase()}`)
+        .then((res) => {
+            return res.json()
+        })
+        .then((res) => {
+            console.log(res)
+            setData(res.rates)
+        })
+
+        
+    }, [currency])
+
+    return data
+}
+
+export default useCurrencyInfo;
